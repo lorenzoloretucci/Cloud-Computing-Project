74f670d90196d81268f5e4c55bdacc2a35fd3d3f
vedatk67 <vkatirci67@gmail.com>
2021-06-07 00:11:24 +0200

    Detect Model Weights Changes

--- backend/Classification_Model/Models.py
+++ backend/Classification_Model/Models.py
@@ -5,19 +5,21 @@ import boto3
 
 class Inception_Model():
    def __init__(self):
-      s3 = boto3.resource('s3')
-      s3.Bucket('clc-project-mlmodels').download_file("best_model_2_inc.h5",'best_model_2_inc.h5')
+      self.s3 = boto3.resource('s3')
+      self.retrieve_modification_date()
+      self.s3.Bucket('clc-project-mlmodels').download_file("best_model_2_inc.h5",'best_model_2_inc.h5')
       self.model = keras.models.load_model("best_model_2_inc.h5",compile=True)
       self.class_names = ['buildings', 'forest', 'glacier', 'mountain', 'sea', 'street']
-      load_model_to_mem = self.return_prediction("/home/ec2-user/image_to_model.jpg")
-
+      load_model_to_mem = self.return_prediction("/home/ec2-user/image_to_model.jpg") ## loads model to main memory
 
    def return_prediction(self,image_name):
-      img = tf.keras.preprocessing.image.load_img(image_name,
-                                                  target_size=(150, 150))
+      img = tf.keras.preprocessing.image.load_img(image_name,target_size=(150, 150))
       img = tf.keras.preprocessing.image.img_to_array(img)
       img = tf.expand_dims(img, 0)/255.0
       prediction = self.model.predict(img)
       predicted_class = np.argmax(prediction, axis = 1)
       return self.class_names[predicted_class[0]]
 
+   def retrieve_modification_date(self):
+      obj = self.s3.Object("clc-project-mlmodels","best_model_2_inc.h5")
+      self.model_update_time = obj.last_modified
--- backend/api_classes/ec2_classification_result.py
+++ backend/api_classes/ec2_classification_result.py
@@ -8,20 +8,35 @@ model = Inception_Model()
 
 class Model_Server(Resource):
     def __init__(self):
+        global model
         super().__init__()
         self.image_parser = reqparse.RequestParser()
         self.image_parser.add_argument("image_name",type=str,help="Name of the image on the S3 service is required",required=True)
+        self.s3= boto3.resource('s3')
+        print(model.model_update_time)
+        if not self.retrieve_model_weights_mdate(model.model_update_time):
+            print("here")
+            model= Inception_Model()
 
 
     def get(self):
         args = self.image_parser.parse_args()
         image_name = args["image_name"]
-
-        s3 = boto3.resource('s3')
-        s3.Bucket('userimages').download_file("preprocesseimages/"+image_name, '/home/ec2-user/image_to_model.jpg')
+        self.s3.Bucket('userimages').download_file("preprocesseimages/"+image_name, '/home/ec2-user/image_to_model.jpg')
         predicted_class = model.return_prediction('/home/ec2-user/image_to_model.jpg')
         hostname = socket.gethostname()
         ip_address = socket.gethostbyname(hostname)
         data = {"url": predicted_class,"local_ip_adress":ip_address}
         return data, 200
+
+
+    def retrieve_model_weights_mdate(self,model_date):
+        obj = self.s3.Object("clc-project-mlmodels", "best_model_2_inc.h5")
+        last_weight_version = obj.last_modified
+        if last_weight_version == model_date:
+            return True
+        else:
+            return False
+
+
 
